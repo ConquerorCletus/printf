09db1182ca04e7f548416ce68c7b1651ef7a377c
hertheyhermee <tiamiyuhayzed@gmail.com>
2023-03-26 09:42:00 -0700

    updated print.f

--- _printf.c
+++ _printf.c
@@ -14,8 +14,8 @@ int _printf(const char *format, ...)
 	int i = 0;
 	int count = 0;
 	int value = 0;
-	
 	va_list args;
+
 	va_start(args, format);
 	int (*f)(va_list);
 
@@ -35,22 +35,18 @@ int _printf(const char *format, ...)
 		{
 			f = specifier(&format[i + 1]);
 			if (f != NULL)
-			{
 				value = f(args);
 				count += value;
 				i = i + 2;
 				continue;
-			}
 
 			if (format[i + 1] == '\0')
 				break;
 			if (format[i + 1] != '\0')
-			{
 				value = write(1, &format[i + 1], 1);
 				count += value;
 				i + i + 2;
 				continue;
-			}
 		}
 	}
 	return (count);
