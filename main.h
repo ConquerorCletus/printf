0c63e6e284ebac594d67fba23e625bb5a84f9db9
ConquerorCletus <smartchibuike2019@gmail.com>
2023-03-28 06:56:28 -0700

    _printf.c

--- main.h
+++ main.h
@@ -6,6 +6,20 @@
 #include <unistd.h>
 
 /**
+ * struct flags - struct containing flags to "turn on"
+ * when a flag specifier is passed to _printf()
+ * @plus: flag for the '+' character
+ * @space: flag for the ' ' character
+ * @hash: flag for the '#' character
+ */
+typedef struct flags
+{
+	int plus;
+	int space;
+	int hash;
+} flags_t;
+
+/**
  * struct specifiers - a structure for specifier
  * @s: specifier string
  * @f: function that prints
